28a9be8d4876d498e13dd648dbbcfa71190dcc92
Kirankumar <kiran1kumar15@gmail.com>
2025-01-16 21:37:11 +0530

    Springboot web

---
+++ learn-spring-boot/src/test/java/com/web/springboot/learn_spring_boot/LearnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.springboot.learn_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
