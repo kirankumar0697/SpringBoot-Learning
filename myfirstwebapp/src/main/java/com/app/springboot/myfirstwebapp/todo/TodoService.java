5f345c1b585340a2380f5b0b5d9a5d045273a0ab
Kirankumar <kiran1kumar15@gmail.com>
2025-01-27 22:18:26 +0530

    update todo

--- myfirstwebapp/src/main/java/com/app/springboot/myfirstwebapp/todo/TodoService.java
+++ myfirstwebapp/src/main/java/com/app/springboot/myfirstwebapp/todo/TodoService.java
@@ -7,6 +7,8 @@ import java.util.function.Predicate;
 
 import org.springframework.stereotype.Service;
 
+import jakarta.validation.Valid;
+
 @Service
 public class TodoService {
 
@@ -31,5 +33,16 @@ public class TodoService {
 	public void deleteTodoById(int id) {
 		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
 		todos.removeIf(predicate);
+	}
+
+	public Todo findById(int id) {
+		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+		Todo todo = todos.stream().filter(predicate).findFirst().get();
+		return todo;
+	}
+
+	public void updateTodo(@Valid Todo todo) {
+		deleteTodoById(todo.getId());
+		todos.add(todo);
 	}
 }
