95a05c7892dc2eff0378675b23a2dca9c78623d7
Kirankumar <kiran1kumar15@gmail.com>
2025-02-03 22:19:42 +0530

    Rest

---
+++ restful-web-services/src/main/java/com/app/rest/webservices/restful_web_services/user/UserDaoService.java
@@ -0,0 +1,39 @@
+package com.app.rest.webservices.restful_web_services.user;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDaoService {
+
+	private static List<User> users = new ArrayList<User>();
+	private static int count = 0;
+	
+	static {
+		users.add(new User(++count, "Jack", LocalDate.now().minusYears(20)));
+		users.add(new User(++count, "Jen", LocalDate.now().minusYears(23)));
+		users.add(new User(++count, "Jolly", LocalDate.now().minusYears(27)));
+	}
+	
+	public List<User> findAllUsers() {
+		return users;
+	}
+	
+	public User findUser(int id) {
+		
+		Predicate<User> predicate = user -> user.getId()== id;
+		User user = users.stream().filter(predicate).findFirst().get();
+		return user;
+	}
+	
+	public User save(User user) {
+		user.setId(++count);
+		users.add(user);
+		return user;
+	}
+}
---
+++ restful-web-services/src/main/java/com/app/rest/webservices/restful_web_services/user/UserResource.java
@@ -0,0 +1,47 @@
+package com.app.rest.webservices.restful_web_services.user;
+
+import java.net.URI;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+public class UserResource {
+
+	private UserDaoService service;
+	
+	public UserResource(UserDaoService service) {
+		super();
+		this.service = service;
+	}
+	
+	@GetMapping("/users")
+	public List<User> retrieveAllUsers() {
+		
+		return service.findAllUsers();
+	}
+	
+	@GetMapping("/users/{id}")
+	public User retrieveUser(@PathVariable int id) {
+		
+		return service.findUser(id);
+	}
+	
+	@PostMapping("/users")
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		
+		User savedUser = service.save(user);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+		return ResponseEntity.created(location).build();
+	}
+	
+}
